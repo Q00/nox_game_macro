9df02d38c2a276b5221f802843fd628e766e2152
kyu <ljk950327@playauto.co.kr>
2018-03-26 19:50:11 +0900

    initial commit

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 120);
+            this.button1.Name = "start";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(235, 159);
+            this.button2.Name = "stop";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(223, 241);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(82, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 373);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string IpClassName, string IpWindowName);
+        [DllImport("user32.dll")]
+        private static extern int FindWindowEx(int hWnd1, int hWnd2, string Ipsz1, string Ipsz2);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hwnd, int wMsg, int wParam, int Iparam);
+        [DllImport("user32.dll")]
+        public static extern bool printWindow(IntPtr hwnd, IntPtr hDc, uint nFlags);
+        [DllImport("user32.dll")]
+        public static extern IntPtr CreateRectRgn(int nLeftRect, int nTopRect, int  nRightRect, int nBottomRect);
+        [DllImport("user32.dll")]
+        public static extern IntPtr getWindowRgn(IntPtr hwnd, IntPtr hRgn);
+
+
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP= 0x0202;
+        const int BM_CLICK = 0x00F5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IntPtr nhwnd = (IntPtr)FindWindow(null, "녹스 플레이어"); // 윈도우 창 제목
+
+            if (!nhwnd.Equals(IntPtr.Zero)){
+                PrintWindow2(nhwnd);
+            }
+
+            //if (nhwnd > 0)
+            //{
+            //    int hw2 = FindWindowEx(nhwnd, 0, "DirectUIHWND", "");
+            //}
+        }
+
+        private static void PrintWindow2(IntPtr hwnd)
+        {
+            DateTime startTime = DateTime.Now;
+            Rectangle rc = Rectangle.Empty;
+            Graphics gfxWin = Graphics.FromHwnd(hwnd);
+            rc = Rectangle.Round(gfxWin.VisibleClipBounds);
+
+            Bitmap bmp = new Bitmap(
+                rc.Width, rc.Height,
+                System.Drawing.Imaging.PixelFormat.Format64bppArgb);
+
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+            bool succeeded = printWindow(hwnd, hdcBitmap, 1);
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            if (!succeeded)
+            {
+                gfxBmp.FillRectangle(
+                        new SolidBrush(Color.Gray),
+                        new Rectangle(Point.Empty, bmp.Size)
+                    );
+            }
+            IntPtr hRgn = CreateRectRgn(0, 0, 0, 0);
+            getWindowRgn(hwnd, hRgn);
+            Region region = Region.FromHrgn(hRgn);
+
+            if (!region.IsEmpty(gfxBmp))
+            {
+                gfxBmp.ExcludeClip(region);
+                gfxBmp.Clear(Color.Transparent);
+            }
+            gfxBmp.Dispose();
+            Console.WriteLine((DateTime.Now - startTime).TotalMilliseconds);
+            bmp.Save(@"c:\\test.bmp",System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
